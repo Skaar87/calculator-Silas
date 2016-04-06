e484ca231f4e2a2bf7bea13e66acc8c50dcc8814
Silas Cagatay <silas.cagatay@nackademin.se>
2016-04-06 15:18:58 +0200

    my first commit

---
+++ src/main/java/calculator.java
@@ -0,0 +1,20 @@
+
+public class calculator {
+	
+	public double add(double x, double y){
+		  
+		return x + y;
+	}
+	public double sub(double x, double y){
+		 
+		return x - y;
+	}
+	public double multi(double x, double y){
+		
+		return  x * y; 
+	}
+	public double div(double x, double y){
+		
+		return x / y;
+	}
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+	
+	public static void main(String[] args) {
+		
+		calculator calc = new calculator();
+		
+		System.out.println("Add: " + calc.add(3.0,5.0));
+		System.out.println("Sub: " + calc.sub(3.0,5.0));
+		System.out.println("Multi: " + calc.multi(3.0,5.0));
+		System.out.println("Div: " + calc.div(3.0,5.0));
+			
+	}
+	
+}
